b43e5a909930e5c7c6ded86249cff4512bb86538
Malcolm <forster.malcolm@gmail.com>
2022-08-20 13:11:21 +1200

    Format changes

--- FishingTrip/Pages/Shared/ForecastFormat.cs
+++ FishingTrip/Pages/Shared/ForecastFormat.cs
@@ -9,7 +9,9 @@ namespace FishingTrip.Pages.Shared
         public string waterTemp = "";        
         public static string[] displayTimes = {"0 AM","3 AM","6 AM","9 AM","12 PM","3 PM","6 PM","9 PM"};
         private Queue<string> waveStrings = new Queue<string>();
+        private Queue<string> wavePWRStrings = new Queue<string>();
         public string[] waveInfo = new string[displayTimes.Length+1];
+        public string[] wavePower = new string[displayTimes.Length + 1];
         //This class is meant to return the information to a page about the forecast of a fishing spot from both Surf-Forecast and Tides4Fishing
         public ForecastFormat(KeyValuePair<string, Hour[]> dayInfo) //Constructor for the _ForecastFormat
         {
@@ -20,20 +22,28 @@ namespace FishingTrip.Pages.Shared
                 if (time.HighTide != "" && time.HighTide +" " != highTides) { highTides = string.Concat(highTides, time.HighTide, " ");}
                 if (time.WavePower != null)
                 {
-                    waveStrings.Enqueue(String.Format("{0} kj / {1} m", time.WavePower, time.WaveHeight));
+                    wavePWRStrings.Enqueue(String.Format("{0} kj", time.WavePower));
                 }
-                else
-                {
-                    waveStrings.Enqueue(String.Format("{0} m", time.WaveHeight));
-                }                    
+                waveStrings.Enqueue(String.Format("{0} m", time.WaveHeight));                   
                     //string.Concat(time.WavePower, " kJ", time.WaveHeight, " m"));;
             }
             //Add infomation to an array
-            waveInfo[0] = "Wave Power/Height:";
+            waveInfo[0] = "Wave Height:";
+            wavePower[0] = "";
             for (int i = 1; i < waveInfo.Length; i++)
             {
                 if(i < 9 - dayInfo.Value.Count()) { waveInfo[i] = "---------"; }
                 else { waveInfo[i] = waveStrings.Dequeue(); }
+            }
+
+            if(wavePWRStrings.Count() > 1)
+            {
+                wavePower[0] = "Wave Power:";
+                for (int i = 1; i < wavePower.Length; i++)
+                {
+                    if (i < 9 - dayInfo.Value.Count()) { wavePower[i] = "---------"; }
+                    else { wavePower[i] = wavePWRStrings.Dequeue(); }
+                }
             }
             waterTemp = @dayInfo.Value.First().WaterTemperature.Replace("[", "").Replace("]", "");
         }
@@ -42,13 +52,15 @@ namespace FishingTrip.Pages.Shared
     public class ForecastDiv
     {
         public string searchSpot { get; set; }
-        public string rawDiv;
+        public string rawDiv;        
         public ForecastDiv(KeyValuePair<string, Hour[]> dayInfo)
         {
+            string concatWavePower = "";
             //this.searchSpot = searchSpot;
             ForecastFormat forecastDiv = new ForecastFormat(dayInfo);
             List<string> times = new List<string>();
             List<string> waveInfo = new List<string>();
+            List<string> wavePower = new List<string>();
 
             foreach (string time in ForecastFormat.displayTimes)
             {
@@ -58,17 +70,26 @@ namespace FishingTrip.Pages.Shared
             {
                 waveInfo.Add("<td>" + s + " </td>");
             }
+            if (forecastDiv.wavePower[0]  == "Wave Power:")
+            {
+                foreach (string s in forecastDiv.wavePower)
+                {
+                    wavePower.Add("<td>" + s + " </td>");
+                }
+                concatWavePower = "</tr><tr>" + String.Concat(wavePower.ToArray());         }
+
 
             string concatTimes = String.Concat(times.ToArray());
-            string concatWaveInfo = String.Concat(waveInfo.ToArray());
+            string concatWaveInfo = String.Concat(waveInfo.ToArray());            
 
             rawDiv = @"<div name = 'forecastDiv' class='forecastDiv' style='display:none' id='" + dayInfo.Key + "'>" +
-                        "<h2 style = 'display: inline-block;'>" + dayInfo.Key + ": " +
-                        "</h2><label>Low tides: " + forecastDiv.lowTides + "</label>" +
-                        "<label>, High tides: " + forecastDiv.highTides + "</label>" +
-                        "<label>, Sea Temperature:" + forecastDiv.waterTemp + "</label>" +
+                        "<h2 class='forecastDay'>" + dayInfo.Key + ": " +                        
+                        "</h2><label>Sea Temperature:" + forecastDiv.waterTemp + "</label>"+
+                        "<label class='blocking'>Low tides: " + forecastDiv.lowTides + "</label>" +
+                        "<label class='blocking'>High tides: " + forecastDiv.highTides + "</label>" +                        
                         "<table class=\"forecastTable\"><tr><th></th>" + concatTimes + "</tr><tr>" +
-                        concatWaveInfo+"</tr></table></div>";
+                        concatWaveInfo+concatWavePower+"</tr></table></div>";
+
         }
     }    
 }
